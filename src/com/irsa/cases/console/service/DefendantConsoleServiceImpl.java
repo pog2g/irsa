101a2611a5e7321d9ea349adaa0c9dfcbf5d2ba1
pog2g <z7630853@163.com>
2019-05-07 17:30:56 +0800

    修复添加被申请人报系统异常(原因：原ps sql字段对应不上)

--- src/com/irsa/cases/console/service/DefendantConsoleServiceImpl.java
+++ src/com/irsa/cases/console/service/DefendantConsoleServiceImpl.java
@@ -1,24 +1,18 @@
 package com.irsa.cases.console.service;
 
-import java.util.List;
-import java.util.Map;
-
+import com.common.utils.Utils;
+import com.irsa.cases.manager.CasesPersonnelManager;
+import com.irsa.cases.manager.DefendantManager;
+import com.irsa.cases.model.*;
+import com.jdbc.utils.Utils4JDBC;
 import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.common.utils.Utils;
-import com.irsa.cases.manager.CasesPersonnelManager;
-import com.irsa.cases.manager.DefendantManager;
-import com.irsa.cases.model.Cases;
-import com.irsa.cases.model.CasesPersonnel;
-import com.irsa.cases.model.CasesPersonnelTemp;
-import com.irsa.cases.model.Defendant;
-import com.irsa.cases.model.DefendantFile;
-import com.irsa.cases.model.TempFile;
-import com.jdbc.utils.Utils4JDBC;
+import java.util.List;
+import java.util.Map;
 
 @Service
 public class DefendantConsoleServiceImpl implements DefendantConsoleService {
@@ -27,7 +21,7 @@ public class DefendantConsoleServiceImpl implements DefendantConsoleService {
     CasesPersonnelManager casesPersonnelManager;
     @Autowired
     DefendantManager defendantManager;
-    
+
     @Override
     public Map<String, Object> getList(String page, String key) {
         try {
@@ -38,12 +32,12 @@ public class DefendantConsoleServiceImpl implements DefendantConsoleService {
                 param = Utils4JDBC.mergeObjectArray(param, new Object[]{"%" + key + "%"});
             }
             sql.append("order by p.account desc,p.type_id,p.name,p.createtime ");
-            
+
             Map<String, Object> result = defendantManager.getPageList(sql.toString(), param, Integer.parseInt(page), 10, "data_list");
             @SuppressWarnings("unchecked")
-            List<Map<String, Object>> dataList = (List<Map<String, Object>>)result.get("data_list");
+            List<Map<String, Object>> dataList = (List<Map<String, Object>>) result.get("data_list");
             if (dataList != null && dataList.size() > 0) {
-                for(Map<String, Object> data : dataList) {
+                for (Map<String, Object> data : dataList) {
                     DefendantManager.getFileUrl(data);
                 }
             }
@@ -56,7 +50,7 @@ public class DefendantConsoleServiceImpl implements DefendantConsoleService {
 
     //保存被申请人到案件
     @Override
-    public Map<String, Object> save4Personnel(String actId, String casesId, String personnelId, String type, String unit_name, String unit_abode, String unit_contact, String name ,String legal_person_type, String duty) {
+    public Map<String, Object> save4Personnel(String actId, String casesId, String personnelId, String type, String unit_name, String unit_abode, String unit_contact, String name, String legal_person_type, String duty) {
         try {
             if (StringUtils.isBlank(type) || "-1".equals(type)) {
                 return Utils.getErrorMap("请选择被申请人类型");
@@ -73,11 +67,11 @@ public class DefendantConsoleServiceImpl implements DefendantConsoleService {
             if (StringUtils.isBlank(name)) {
                 return Utils.getErrorMap("请填写法定代表人");
             }
-            
+
             Map<String, Object> tempFile1 = defendantManager.getMap(TempFile.SELECT_BY_RESID_MODE, new Object[]{actId, DefendantFile.MODE_1});
             Map<String, Object> tempFile2 = defendantManager.getMap(TempFile.SELECT_BY_RESID_MODE, new Object[]{actId, DefendantFile.MODE_2});
             Map<String, Object> tempFile3 = defendantManager.getMap(TempFile.SELECT_BY_RESID_MODE, new Object[]{actId, DefendantFile.MODE_3});
-            
+
             String pid = "";
             if (StringUtils.isNotBlank(personnelId)) {
                 pid = personnelId;
@@ -87,7 +81,7 @@ public class DefendantConsoleServiceImpl implements DefendantConsoleService {
                     pid = Utils.toString(defendant.get("id"));
                 }
             }
-            
+
             // 案件是否存在
             int isCasesExist = defendantManager.getListCount(Cases.SELECT_EXIST_BY_ID, new Object[]{casesId});
             if (StringUtils.isNotBlank(pid)) {
@@ -104,14 +98,14 @@ public class DefendantConsoleServiceImpl implements DefendantConsoleService {
                     }
                 }
 
-               /* update irsa_defendant set type_id = ?, unit_name = ?, unit_content = ?, unit_abode = ?, name = ?, legal_person_type = ?, duty = ? where id = ?*/
+                /* update irsa_defendant set type_id = ?, unit_name = ?, unit_content = ?, unit_abode = ?, name = ?, legal_person_type = ?, duty = ? where id = ?*/
                 defendantManager.executeSQL(Defendant.UPDATE, new Object[]{type, unit_name, unit_contact, unit_abode, name, legal_person_type, duty, personnelId});
             } else {
                 personnelId = Utils.getId();
                 /*insert into irsa_defendant(id,createtime,type_id,unit_name,unit_content,unit_abode,name,legal_person_type,duty,account,pwd)*/
-                defendantManager.executeSQL(Defendant.INSERT, new Object[]{personnelId, Utils.getCreateTime(), type , unit_name, name, unit_contact, unit_abode, name, legal_person_type, duty, "", ""});
-            }    
-            
+                defendantManager.executeSQL(Defendant.INSERT, new Object[]{personnelId, Utils.getCreateTime(), type, unit_name, unit_contact, unit_abode, name, legal_person_type, duty, "", ""});
+            }
+
             if (tempFile1 != null) {
                 defendantManager.saveFile(personnelId, DefendantFile.MODE_1, tempFile1);
             }
@@ -121,14 +115,14 @@ public class DefendantConsoleServiceImpl implements DefendantConsoleService {
             if (tempFile3 != null) {
                 defendantManager.saveFile(personnelId, DefendantFile.MODE_3, tempFile3);
             }
-            
+
             // 案件是否存在
             if (isCasesExist == 0) {
 //                defendantManager.executeSQL(CasesPersonnelTemp.DELETE_BY_TYPE, new Object[]{casesId, CasesPersonnel.PERSONNEL_TYPE_2});
                 defendantManager.executeSQL(CasesPersonnelTemp.INSERT, new Object[]{Utils.getId(), Utils.getCreateTime(), CasesPersonnel.PERSONNEL_TYPE_2, casesId, personnelId, null});
                 return Utils.getSuccessMap(null);
             }
-            
+
             casesPersonnelManager.save4Defendant(casesId, personnelId);
             return Utils.getSuccessMap(null);
         } catch (Exception e) {
@@ -136,15 +130,16 @@ public class DefendantConsoleServiceImpl implements DefendantConsoleService {
             return Utils.getErrorMap(null);
         }
     }
-    
+
     @Override
-    public Map<String,Object> getChooseList(String page, String key) {
+    public Map<String, Object> getChooseList(String page, String key) {
         try {
             StringBuffer sql = new StringBuffer(Defendant.SELECT_4_CHOOSE);
-            Object[] param = new Object[]{"%" + Utils.changeISO88591ToUTF8(key) + "%"};;
+            Object[] param = new Object[]{"%" + Utils.changeISO88591ToUTF8(key) + "%"};
+            ;
             return Utils.getSuccessMap(defendantManager.getPageList(sql.toString(), param, Integer.parseInt(page), 10, "data_list"));
-        } catch(Exception e) {
-            log.error(e.getMessage(),e); 
+        } catch (Exception e) {
+            log.error(e.getMessage(), e);
             return Utils.getErrorMap(null);
         }
     }
@@ -154,12 +149,12 @@ public class DefendantConsoleServiceImpl implements DefendantConsoleService {
     @Override
     public Map<String, Object> save(String resId, String id, String typeId, String unit_name, String unit_abode, String unit_contact, String name, String legal_person_type, String duty, String account, String pwd, String pwdConfirm) {
         try {
-            
+
             int num = defendantManager.getListCount(Defendant.SELECT_EXIST_BY_ID, new Object[]{id});
             if (num == 0) {
                 return Utils.getErrorMap(null);
             }
-            
+
             if (StringUtils.isBlank(typeId) || "-1".equals(typeId)) {
                 return Utils.getErrorMap("请选择被申请人类型");
             }
@@ -182,7 +177,7 @@ public class DefendantConsoleServiceImpl implements DefendantConsoleService {
             if (num != 0) {
                 return Utils.getErrorMap("用户名已存在");
             }
-            
+
             if (StringUtils.isBlank(pwd)) {
                 /*update irsa_defendant set type_id = ?,unit_name = ?, unit_content = ?, unit_abode = ?, name = ?, account = ? where id = ?*/
                 defendantManager.executeSQL(Defendant.UPDATE_1, new Object[]{typeId, unit_name, unit_contact, unit_abode, name, account, id});
@@ -208,8 +203,8 @@ public class DefendantConsoleServiceImpl implements DefendantConsoleService {
                 defendantManager.saveFile(id, DefendantFile.MODE_3, tempFile3);
             }
             return Utils.getSuccessMap(null);
-        } catch(Exception e) {
-            log.error(e.getMessage(),e); 
+        } catch (Exception e) {
+            log.error(e.getMessage(), e);
             return Utils.getErrorMap(null);
         }
     }
@@ -217,12 +212,15 @@ public class DefendantConsoleServiceImpl implements DefendantConsoleService {
     public CasesPersonnelManager getCasesPersonnelManager() {
         return casesPersonnelManager;
     }
+
     public void setCasesPersonnelManager(CasesPersonnelManager casesPersonnelManager) {
         this.casesPersonnelManager = casesPersonnelManager;
     }
+
     public DefendantManager getDefendantManager() {
         return defendantManager;
     }
+
     public void setDefendantManager(DefendantManager defendantManager) {
         this.defendantManager = defendantManager;
     }
