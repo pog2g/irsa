6a6c55e7a0dcf2fea81f5637cee8778fb5e5820c
pog2g <z7630853@163.com>
2019-05-12 01:14:54 +0800

    修改人员没有叠加显示

--- src/com/irsa/cases/model/CasesPersonnelTemp.java
+++ src/com/irsa/cases/model/CasesPersonnelTemp.java
@@ -2,36 +2,36 @@ package com.irsa.cases.model;
 
 /**
  * 案件相关人员-临时
- * @author EZ
  *
+ * @author EZ
  */
 public class CasesPersonnelTemp {
     String irsa_cases_personnel_temp;
     String id;
     String createtime;
-    
+
     String type;// 参考CasesPersonnel的personnel_type字段
     String cases_id;
     String personnel_id;
     String client_id;// 委托人：
     String representative; // 是否为申请人代表，0=否，1=是
-    
+
     public static String INSERT = "insert into irsa_cases_personnel_temp(id,createtime,type,cases_id,personnel_id,client_id) values(?,?,?,?,?,?)";
     public static String UPDATE_REPRESENTATIVE = "update irsa_cases_personnel_temp set representative = ? where id = ?";
-    
+
     public static String SELECT_EXIST = "select id,type,personnel_id,client_id,representative from irsa_cases_personnel_temp ";
     public static String SELECT_EXIST_BY_CASESID_PERSONNELID = SELECT_EXIST + "where cases_id = ? and personnel_id = ?";
     public static String SELECT_EXIST_BY_CASESID_TYPE = SELECT_EXIST + "where cases_id = ? and type = ? order by createtime";
     public static String SELECT_EXIST_BY_CASESID_REPRESENTATIVE = SELECT_EXIST + "where cases_id = ? and type = ? and representative = ? order by createtime,personnel_id";
     public static String SELECT_EXIST_BY_ID = SELECT_EXIST + "where id = ?";
     public static String SELECT_EXIST_BY_CASESID_PERSONNELID_CLIENTID = SELECT_EXIST + "where cases_id = ? and personnel_id = ? and client_id = ?";
-    
+
     public static String DELETE_BY_ID = "delete from irsa_cases_personnel_temp where id = ?";
     public static String DELETE_BY_TYPE = "delete from irsa_cases_personnel_temp where cases_id = ? and type = ?";
-    
+
     public static String SELECT_CHOOSE_BY_CASESID_TYPE_1 = "select id," +
-    		"(select p.name from irsa_party p where p.id = t.personnel_id) text " +
-    		"from irsa_cases_personnel_temp t where cases_id = ? and type = '1' order by createtime";
+            "(select p.name from irsa_party p where p.id = t.personnel_id) text " +
+            "from irsa_cases_personnel_temp t where cases_id = ? and type = '1' order by createtime";
     
    /* public static String SELECT_PARYT_BY_CASESID = "select t.id,t.type personnel_type,t.personnel_id,t.representative," +
             "p.type,p.name,p.legal_person,p.gender,date_format(p.birthday,'%Y-%m-%d') birthday,p.phone,p.contact,p.zip_code,p.county_id,p.address,p.id_type_id,p.id_no,p.unit_id_type_id,p.unit_id_no,p.unit_contact,p.domicile," +
@@ -62,10 +62,11 @@ public class CasesPersonnelTemp {
             "(select f.real_name from irsa_party_file f where f.res_id = p.id) id_file_name," +
             "(select f.file_name from irsa_party_file f where f.res_id = p.id) id_file " +
             "from irsa_cases_personnel_temp t,irsa_party p where p.trash = '0' and t.personnel_id = p.id ";
-    public static String SELECT_BY_CASESID_TYPE_137 = SELECT_PARYT_BY_CASESID + "and t.cases_id = ? and t.type = ? order by t.createtime desc limit 0,1";
-    public static String SELECT_BY_CASESID_REPRESENTATIVE = SELECT_PARYT_BY_CASESID + 
+    //    public static String SELECT_BY_CASESID_TYPE_137 = SELECT_PARYT_BY_CASESID + "and t.cases_id = ? and t.type = ? order by t.createtime desc limit 0,1";
+    public static String SELECT_BY_CASESID_TYPE_137 = SELECT_PARYT_BY_CASESID + "and t.cases_id = ? and t.type = ? order by t.createtime desc";
+    public static String SELECT_BY_CASESID_REPRESENTATIVE = SELECT_PARYT_BY_CASESID +
             "and t.cases_id = ? and t.type = ? and t.representative = ? order by t.createtime,t.personnel_id";
-    
+
     public static String SELECT_BY_CASESID_TYPE_2 = "select t.id,t.type personnel_type,t.personnel_id," +
             "p.type_id type,p.name,p.legal_person_type,p.unit_name,p.unit_abode,p.unit_contact,p.duty," +
             "(select t.title from irsa_defendant_type t where t.id = p.type_id) label_type," +
@@ -88,7 +89,6 @@ public class CasesPersonnelTemp {
             "and t.cases_id = ? and t.type = '2' order by t.createtime";
 
 
-    
     public static String SELECT_BY_CASESID_CLIENTID = "select t.id,t.type personnel_type,t.personnel_id," +
             "p.name,p.unit_name,p.gender,date_format(p.birthday,'%Y-%m-%d') birthday,p.phone,p.domicile,p.id_type_id,p.id_no,p.identity," +
             "case p.gender when '1' then '男' else '女' end label_gender," +
